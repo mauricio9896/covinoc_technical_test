2c76233ca5bbd72e30f5d064e4e2405ca737a873
Mauricio Buitrago <mauricio9896@hotmail.com>
2024-06-20 08:48:46 -0500

    layouting and creating componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './components/page/page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormTaskComponent } from './components/form-task/form-task.component';
+import { ListTaskComponent } from './components/list-task/list-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    FormTaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
